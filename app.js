71128872989de54021fda1a2b1fbafd960c0c8e2
Kaijun <kaijun_huang@outlook.com>
2023-02-09 10:49:18 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const restoList = require("./restaurant.json");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { resto: restoList.results });
+});
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  const restaurant = restoList.results.find(
+    (item) => item.id.toString() === id
+  );
+  res.render("show", { resto: restaurant });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const searchedResto = restoList.results.filter((item) =>
+    item.name.toLowerCase().includes(keyword.toLowerCase())
+  );
+  console.log(searchedResto);
+  res.render("index", { resto: searchedResto, keyword: keyword });
+});
+
+app.listen(port, () => {
+  console.log(`My page is running on http://localhost/${port}`);
+});
