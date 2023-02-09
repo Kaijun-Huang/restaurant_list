46556bb8dd9be2b4efecff7f18c03f6dd3f541a8
Kaijun <kaijun_huang@outlook.com>
2023-02-09 12:15:26 +0800

    no message

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.get("/search", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`The page is running on http://localhost/${port}`);
+  console.log(`The page is running on http://localhost:${port}`);
 });
